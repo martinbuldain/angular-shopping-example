b6869bf003267fdaa0ae6a2399c2a082fb32caca
martin <martinbuldain@gmail.com>
2018-07-06 18:32:26 -0300

    First commit

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient {
+    /*public name: string;
+    public amount: number;
+
+    constructor(name: string, amount: number) {
+        this.name = name;
+        this.amount = amount;
+    }*/
+
+    // Otra forma de declarar el contructor que hace lo mismo que lo de arriba
+    constructor(public name: string, public amount: number) {}
+}
